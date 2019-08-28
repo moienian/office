ca53375caedf15dd7e8bf3eecc0a17418a034c3a
Mohammad Moien <mnmoienian@gmail.com>
2019-08-28 09:28:23 +0430

    config webpack for handle sourcemap

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const template = <p>chetori?</p>;
-// const template = React.createElement("p", {}, "salam");
 const root = document.getElementById("root");
 
 ReactDOM.render(template, root);
