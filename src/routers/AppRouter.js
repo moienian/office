0c8351a7a70eda154c61d792054a8d4e4f72fc5f
Mohammad Moien <mnmoienian@gmail.com>
2019-09-01 08:33:11 +0430

    add route for 404 page

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import DashboardPage from "../components/DashboardPage";
+import NotFoundPage from "../components/NotFoundPage";
 
 const AppRouter = () => (
   <Router>
     <Switch>
       <Route path="/" component={DashboardPage} exact={true} />
+      <Route component={NotFoundPage} />
     </Switch>
   </Router>
 );
