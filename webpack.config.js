d091ac0954286ef74337409d05eee4a7e767e488
Mohammad Moien <mnmoienian@gmail.com>
2019-10-31 08:54:41 +0330

    edit webpack config to open page after run dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,8 @@ module.exports = {
   devtool: "cheap-module-eval-source-map",
   devServer: {
     contentBase: path.join(__dirname, "public"),
-    historyApiFallback: true
+    watchContentBase: true,
+    publicPath: "/dist/",
+    open: true
   }
 };
