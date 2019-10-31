f51d18443cd101e2764d15811cb75487cf8e28d9
Mohammad Moien <mnmoienian@gmail.com>
2019-10-31 10:34:39 +0330

    config jest

---
+++ src/__tests__/DashboardPage.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import DashboardPage from "../components/DashboardPage";
+
+afterEach(() => {
+  cleanup();
+});
+
+test("render DashboardPage component correctly", () => {
+  const { getByText } = render(<DashboardPage />);
+  expect(getByText("this is from dashboard page!")).toBeInTheDocument();
+});
